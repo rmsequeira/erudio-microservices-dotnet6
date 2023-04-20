3f1102dd43e34b89dbce7a1befc4a2057c2f36c2
rmsequeira <ricardo.sequeira@gmail.com>
2023-04-20 21:10:20 +0100

    Preparacao de Microservicos WEB para consumo do microservicos de Produtos

---
+++ S09_Preparacao de Microservicos WEB para consumo do microservicos de Produtos/GeekShoping/GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,90 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
+using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace GeekShopping.ProductAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+
+            builder.Services.AddDbContext<MySQLContext>(options => options.
+                UseMySql(connection,
+                new MySqlServerVersion(new Version(8, 0, 32))));
+
+            IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+
+            builder.Services.AddSingleton(mapper);
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            //Dependecy Injection
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "v1" });
+            });
+
+
+
+            var app = builder.Build();
+
+
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GeekShopping.ProductAPI v1"));
+            }
+            app.UseRouting();
+
+
+            app.UseAuthorization();
+
+            //    var summaries = new[]
+            //    {
+            //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            //};
+
+            //    app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            //    {
+            //        var forecast = Enumerable.Range(1, 5).Select(index =>
+            //            new WeatherForecast
+            //            {
+            //                Date = DateTime.Now.AddDays(index),
+            //                TemperatureC = Random.Shared.Next(-20, 55),
+            //                Summary = summaries[Random.Shared.Next(summaries.Length)]
+            //            })
+            //            .ToArray();
+            //        return forecast;
+            //    })
+            //    .WithName("GetWeatherForecast");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+
+            app.Run();
+        }
+    }
+}
